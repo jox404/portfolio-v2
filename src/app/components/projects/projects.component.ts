03330bd42f030ca424e2a4e9b70a0389933ad818
jox404 <joaovitorribeiroc1@gmail.com>
2023-01-02 14:13:42 -0300

    add more projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -8,6 +8,7 @@ type project = {
   description: string;
   technologies: string[];
   background: string;
+  links: { pageOnline: string | undefined; repository: string | undefined };
 };
 
 @Component({
@@ -36,6 +37,10 @@ export class ProjectsComponent {
         'Uma wiki de animes, nela é possível buscar informações de diversos animes, criar uma conta e classificar esses animes colocados em lista de favoritos, acompanhado, dropados etc. Sua base foi construída usando react, next e consumindo a API da Kitsu, as demais ferramentas de login favoritar os anime etc... vem do firebase. foi um projeto que demandou muito esforço e que no seu fim me deu muita confiança e me deixou muito feliz com o resultado.',
       technologies: ['React', 'Firebase', 'Next', 'REST'],
       background: 'animeJox06.png',
+      links: {
+        pageOnline: 'https://anime-jox.vercel.app/',
+        repository: 'https://github.com/jox404/anime-jox',
+      },
     },
     {
       name: 'Bolsa De Atletas',
@@ -43,6 +48,7 @@ export class ProjectsComponent {
         'Uma Landing page, para a apresentação e divulgação de um serviço. não posso divulgar o código, porque não tenho a autorização da empresa que contratou o serviço.',
       technologies: ['React', 'Material-UI', 'CSS', 'JS'],
       background: 'bolsaDeAtletas01.png',
+      links: { pageOnline: undefined, repository: undefined },
     },
     {
       name: 'WeatherApp',
@@ -58,6 +64,32 @@ export class ProjectsComponent {
         'mySQL',
       ],
       background: 'weather-app.png',
+      links: {
+        pageOnline: undefined,
+        repository: 'https://github.com/jox404/weather-app',
+      },
+    },
+    {
+      name: 'Photographer Portfolio',
+      description:
+        'Um portfólio de um fotógrafo, apresentando um album de fotos e serviços.Quando o fiz esse projeto meu objetivo principa era exercitar minhas habilidades com animações CSS',
+      technologies: ['TypeScript', 'React', 'CSS'],
+      background: 'photographerPortfolio.png',
+      links: {
+        pageOnline: 'https://jox404.github.io/photographer-portfolio/',
+        repository: 'https://github.com/jox404/photographer-portfolio',
+      },
+    },
+    {
+      name: 'Portfolio(Versão Anterio)',
+      description:
+        'Essa é a versão anterior do meu meu portfólio, criada em 2022.',
+      technologies: ['JS', 'React', 'CSS'],
+      background: 'portfolio2022.png',
+      links: {
+        pageOnline: 'https://jox404.github.io/portifolio/',
+        repository: 'https://github.com/jox404/portifolio',
+      },
     },
   ];
 }
